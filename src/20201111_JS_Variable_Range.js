fb4b014e079e27e4674f781f66c6eea531c2611b
rubyviolin <72783602+rubyviolin@users.noreply.github.com>
2020-11-11 11:17:51 +0900

    2020 JS Practice

--- src/20201111_JS_Variable_Range.js
+++ src/20201111_JS_Variable_Range.js
@@ -10,14 +10,16 @@ let globalLetVariable = "global use let";
 const globalConstVariable = "global const type";
 
 function func() {
-  console.log("CALL FUNC.");
+  console.log("===== CALL FUNC.======");
 
   // 함수 내에서 로컬 변수에 대한 접근
   var localVarVariable = "local use var";
   let localLetVariable = "local use let";
   const localConstVariable = "local const type";
 
-  // 함수 내에서 전역변수와 동일한 이름의 지역변수를 사용하는 경우.
+  // 함수 내에서 전역변수와 동일한 이름의 지역변수를
+  // 사용하는 경우에는 주의해야 합니다.
+  // 전역변수를 사용할 수 없게 됩니다.
   var globalVarVariable = "not global use var";
   let globalLetVariable = "not global use let";
   const globalConstVariable = "not global const type";
@@ -38,4 +40,6 @@ console.log(globalLetVariable);
 console.log(globalConstVariable);
 
 //함수 밖에서 지역 변수에 대한 접근
-console.log(localVarVariable); // Error
+//console.log(localVarVariable); // Error
+
+console.log("===============");
